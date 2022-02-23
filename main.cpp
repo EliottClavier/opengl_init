4947d69135b3c78f551059944d9e8d71b48003f0
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-02-23 14:29:07 +0100

    Ajout shaders Vertex et Fragment et affichage vertices

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+// Source du shader de vertex par défaut en C (Shader de positionnement dans un Normalized Device Coordinates)
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
@@ -23,6 +42,18 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
+
+struct Vertice {
+    float x;
+    float y;
+    float z;
+};
+
+struct Vertex {
+    Vertice a;
+    Vertice b;
+    Vertice c;
+};
 
 int main()
 {
@@ -50,13 +81,101 @@ int main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Intégration du shader de vertex pour OpenGL
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Intégration du shader de fragment (colorisation des pixels)
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Ajout des deux shaders au programme
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Suppression des shaders (inclus dans le programme)
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Création du vertex
+    /*
+    std::vector<Vertice> vertices{
+        Vertice {-0.5f, -0.5f, 0.0f,},
+        Vertice {0.5f, -0.5f, 0.0f,},
+        Vertice {0.0f,  0.5f, 0.0f},
+    };
+    */
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // Link Vertex au VBO qui va rendre les vertices avec le shader setup au dessus
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertice), &vertices[0], GL_STATIC_DRAW);      
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Création d'un VAO (Vertex Array Object)
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // Utilisation du VAO courant pour la première fois
+    glBindVertexArray(VAO);
+
+    // Configuration pour dire à OpenGL comment interpréter le tableau de vertices
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         // rendering commands here
-        glClearColor(0.2f, 0.9f, 0.3f, 1.0f);
+
+        // gestion de la couleur du buffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Utilisation du programme de shaders
+        glUseProgram(shaderProgram);
+
+        // Utilisation du VAO courant
+        glBindVertexArray(VAO);
+
+        // Dessin du triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         //
 
         glfwSwapBuffers(window);
