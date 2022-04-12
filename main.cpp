eda86cc62f14cfcaae6d8a91d84b0a79374a513f
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-04-12 18:37:06 +0200

    Modification shader avec ajout des couleurs dans les vertices + setup class Shader et fichiers shaders à part

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
+#include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -11,18 +12,64 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // Source du shader de vertex par défaut en C (Shader de positionnement dans un Normalized Device Coordinates)
+// Shader qui communique la couleur entre eux avec vertexColor out et in
+/*
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.96f, 0.96f, 0.35f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
+"}\n\0";
+*/
+
+// Utilisateur uniform
+/*
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = ourColor;\n"
+"}\n\0";
+*/
+
+// location 0 (3 premieres valeurs float pour un vertex) = position
+// location 1 (3 valeurs suivantes float pour un vertex) = couleur
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   ourColor = aColor;\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0);\n"
 "}\n\0";
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
@@ -133,6 +180,7 @@ int main()
     };
     */
 
+    /*
     float vertices[] = {
           0.0f,  -0.8f,  0.0f,
           0.4f,   0.0f,  0.0f,
@@ -145,6 +193,22 @@ int main()
          -0.4f,   0.0f,  0.0f,
           0.0f,   0.8f,  0.0f,
           0.4f,   0.0f,  0.0f,
+    };
+    */
+
+    float vertices[] = {
+          // Position           // Couleur
+          0.0f,  -0.8f,  0.0f,  1.0f, 0.0f, 0.0f,
+          0.4f,   0.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+          0.8f,  -0.8f,  0.0f,  0.0f, 0.0f, 1.0f,
+
+          0.0f,  -0.8f,  0.0f,  1.0f, 0.0f, 0.0f,
+         -0.4f,   0.0f,  0.0f,  0.0f, 1.0f, 0.0f,
+         -0.8f,  -0.8f,  0.0f,  0.0f, 0.0f, 1.0f,
+
+         -0.4f,   0.0f,  0.0f,  1.0f, 0.0f, 0.0f,
+          0.0f,   0.8f,  0.0f,  0.0f, 1.0f, 0.0f,
+          0.4f,   0.0f,  0.0f,  0.0f, 0.0f, 1.0f,
     };
 
     int verticesNumber = { std::size(vertices)};
@@ -194,8 +258,16 @@ int main()
     glBindVertexArray(VAO);
 
     // Configuration pour dire à OpenGL comment interpréter le tableau de vertices
+    // Ici on a 0 qui indique la location dans le shader, 3 le nombre de float par vertice et 3* le stride (l'espace à parcourir dans le tableau entre deux vertices)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // Dans ce cas, on passe à 6 car chaque vertice est composé de 6 floats, 3 positions et 3 couleurs dans le stride est plus grand
+    // On a le vertex attrib pointer location 0 pour la position, et location 1 pour la couleur
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -206,9 +278,17 @@ int main()
         // gestion de la couleur du buffer
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        
         // Utilisation du programme de shaders
         glUseProgram(shaderProgram);
+
+        // Génération random d'une couleur en fonction du temps
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        // On précise que cette couleur doit être appliqué à ourColor dans l'uniform du fragment shader
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        // Utilisateur de la couleur random dans les shaders
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
         // Utilisation du VAO courant
         glBindVertexArray(VAO);
